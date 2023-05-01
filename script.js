0337eab520dc6346ca93fa76e7623ab27f5359f9
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-01 13:22:05 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "🎉 Correct Number";
+
+console.log(document.querySelector(".number").textContent);
+document.querySelector(".number").textContent = 18;
