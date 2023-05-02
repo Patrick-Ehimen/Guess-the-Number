c4ae7dab1883a7786eb717f92155bcb81252338e
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-02 09:54:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 "use strict";
 
-console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "🎉 Correct Number";
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "🎉 Correct Number";
 
-console.log(document.querySelector(".number").textContent);
-document.querySelector(".number").textContent = 18;
+// console.log(document.querySelector(".number").textContent);
+// document.querySelector(".number").textContent = 18;
 
-document.querySelector(".score").textContent = 100;
+// document.querySelector(".score").textContent = 100;
 
-document.querySelector(".guess").value = 20;
+// document.querySelector(".guess").value = 20;
