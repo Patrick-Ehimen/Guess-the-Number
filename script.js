38986f0041fcf860bd74ebda364287618eb8da5b
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-02 10:44:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,23 @@
 // document.querySelector(".guess").value = 200;
 // console.log(document.querySelector(".guess").value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+// document.querySelector(".again").addEventListener('click');
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-
   console.log(guess, typeof guess);
 
-  // document.querySelector(".message").textContent = "🎉 Correct Number";
+  if (!guess) {
+    document.querySelector(".message").textContent = "🚫 No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "📈 Number too high";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "📉 Number too low";
+  }
+  // document.querySelector(".message").textContent = "🎉 Correct secretNumber";
 });
