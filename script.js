2ed7f9047af0b8f680421fd5eb63b08bd4486bbb
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-02 10:17:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,13 @@
 
 // document.querySelector(".score").textContent = 100;
 
-document.querySelector(".guess").value = 20;
-console.log(document.querySelector(".guess").value);
+// document.querySelector(".guess").value = 200;
+// console.log(document.querySelector(".guess").value);
 
 document.querySelector(".check").addEventListener("click", function () {
-  console.log(document.querySelector(".guess").value);
+  const guess = Number(document.querySelector(".guess").value);
+
+  console.log(guess, typeof guess);
+
+  // document.querySelector(".message").textContent = "🎉 Correct Number";
 });
